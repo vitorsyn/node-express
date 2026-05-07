e5fee927d444ed98961edcbba563ec05447f0372
Aluno Lauro de Freitas <aluno.lauro@senaibahia.com.br>
2026-05-06 21:06:50 -0300

    erro no router(outro) fixado

--- routes/produtoRoutes.js
+++ routes/produtoRoutes.js
@@ -6,7 +6,7 @@ router.get('/', controller.listar);
 router.get('/:id', controller.buscarPorId);
 router.post('/', controller.criar);
 router.put('/:id', controller.atualizar);
-router.patch('/produtos/desconto/:pct', controller.aplicarDesconto);
+router.patch('/desconto/:pct', controller.aplicarDesconto);
 router.delete('/:id', controller.deletar);
 
 module.exports = router;
